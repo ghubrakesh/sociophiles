f7e55bc9d7d2e843104cd0827851a6e5b44df4fa
ghubrakesh <sabalerakesh143@gmail.com>
2023-10-18 01:51:19 +0530

    added static files

--- sociophiles/core/views.py
+++ sociophiles/core/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h2>Welcome to Sociophiles!</h2>')
+    return render(request, 'index.html')
